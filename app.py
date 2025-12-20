ddca998bc4063e2856b6e4d8b04d67bfd4ede5e3
Habibur Rahman Zihad <e241024@ugrad.iiuc.ac.bd>
2025-12-20 20:40:50 +0600

    make the app.py

---
+++ app.py
@@ -0,0 +1,42 @@
+import streamlit as st
+from utils.data_manager import DataManager
+from utils.ui_manager import UIManager
+from utils.auth_manager import AuthManager
+from views.dashboard import show_dashboard
+from views.report import show_report_page
+from views.admin import show_admin_page
+from views.my_reports import show_my_reports_page
+from views.auth import show_login_page, show_register_page
+
+# 1. Page Configuration (Must be first)
+st.set_page_config(
+    page_title="NagarNirman", 
+    page_icon="favicon.png", 
+    layout="wide",
+    initial_sidebar_state="collapsed"  # Hide sidebar completely
+)
+
+# 2. Initialize Data & Auth & Theme
+DataManager.init_db()
+AuthManager.init_session()
+UIManager.init_theme()
+UIManager.load_css()
+
+# 3. Render Navbar
+current_page = UIManager.render_navbar()
+
+# 4. Route to appropriate page based on navbar selection
+if current_page == "home":
+    show_dashboard()
+elif current_page == "login":
+    show_login_page()
+elif current_page == "register":
+    show_register_page()
+elif current_page == "my_reports":
+    show_my_reports_page()
+elif current_page == "submit_report":
+    show_report_page()
+elif current_page == "admin":
+    show_admin_page()
+else:
+    show_dashboard()  # Default page
